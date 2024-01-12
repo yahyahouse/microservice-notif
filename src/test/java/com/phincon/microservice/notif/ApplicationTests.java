9760c6e6c77bdb020204695f15fd1434e63b925b
yahya wijaya <yahyawijaya082@gmail.com>
2024-01-12 09:59:11 +0700

    first commit

---
+++ src/test/java/com/phincon/microservice/notif/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phincon.microservice.notif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
