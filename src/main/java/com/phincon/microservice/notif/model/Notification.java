c72ca84cb4e1a57960f72c0b9135c1cfabe9e322
yahya wijaya <yahyawijaya082@gmail.com>
2024-01-15 17:05:34 +0700

    feat : add getter setter manual and constructor

--- src/main/java/com/phincon/microservice/notif/model/Notification.java
+++ src/main/java/com/phincon/microservice/notif/model/Notification.java
@@ -2,21 +2,57 @@ package com.phincon.microservice.notif.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.*;
 
 import java.sql.Timestamp;
 
-@Data
+
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class Notification {
-    @Getter
-    @Setter
     @Id
     String id;
     String orderId;
     String status;
     Timestamp createdDate;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Notification(String id, String orderId, String status, Timestamp createdDate) {
+        this.id = id;
+        this.orderId = orderId;
+        this.status = status;
+        this.createdDate = createdDate;
+    }
+
+    public Notification() {
+    }
 }
--- src/main/java/com/phincon/microservice/notif/model/Orders.java
+++ src/main/java/com/phincon/microservice/notif/model/Orders.java
@@ -2,10 +2,42 @@ package com.phincon.microservice.notif.model;
 
 import lombok.Data;
 
-@Data
+
 public class Orders {
     String id;
     String productId;
     String status;
     Long price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
 }
